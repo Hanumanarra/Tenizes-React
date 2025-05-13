9ce41be9d7214c04eaceac0bf46c8a9e432a6b18
Hanumanarra <151939652+Hanumanarra@users.noreply.github.com>
2025-05-13 12:34:07 +0530

    Create words.js

---
+++ words.js
@@ -0,0 +1,23 @@
+export const words = [
+  "react",
+  "node",
+  "python",
+  "javascript",
+  "css",
+  "html",
+  "docker",
+  "redux",
+  "express",
+  "mongodb",
+  "sql",
+  "typescript",
+  "kotlin",
+  "java",
+  "swift",
+  "angular",
+  "vue",
+  "linux",
+  "bash",
+  "webpack"
+  
+];
